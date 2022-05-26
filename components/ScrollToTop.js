742ade2f152c45f73d1ca68734440adc0ed7a630
Josh Koter <josh.koter@gmail.com>
2022-05-26 08:45:47 +0200

    Fixed scroll to top function and tweaked navbar styles

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -2,31 +2,25 @@ import { useEffect, useState } from 'react'
 import { BsFillArrowUpCircleFill } from 'react-icons/bs'
 import style from '../styles/scroll.module.css'
 
-// this isn't working because it's built to work with scroll on body element,
-// not with scroll on a specific element
-
-export default function ScrollToTop(params) {
-  console.log(params.element.current)
-  const elem = params.element.current
-
+export default function ScrollToTop(elem) {
   const [isVisible, setVisible] = useState(false)
 
-  const toggleVisbility = (e) => {
-    if (elem.scrollTop > 300) setVisible(true)
+  const toggleVisbility = () => {
+    if (elem.element.current.scrollTop > 300) setVisible(true)
     else setVisible(false)
   }
 
   const scrollToTop = () => {
-    elem.scrollTo({
+    elem.element.current.scrollTo({
       top: 0,
       behavior: 'smooth',
     })
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', toggleVisbility)
+    elem.element.current.addEventListener('scroll', toggleVisbility)
     return () => {
-      window.removeEventListener('scroll', toggleVisbility)
+      elem.element.current.removeEventListener('scroll', toggleVisbility)
     }
   })
 
