ccbdd531038692b7689864081a74dfb0cdd6fc0d
Josh Koter <josh.koter@gmail.com>
2022-06-01 22:08:51 +0200

    Upgrades people, upgrades.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,13 @@
 import Link from 'next/link'
 import style from '../styles/navbar.module.css'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function Navbar() {
-  const [isVisible, setVisible] = useState(false)
   const [hamburgerOpen, setHamburgerOpen] = useState(false)
-
-  const toggleVisbility = () => {
-    if (window.pageYOffset > 100) setVisible(true)
-    else setVisible(false)
-  }
 
   const toggleHamburger = () => {
     setHamburgerOpen(!hamburgerOpen)
   }
-
-  useEffect(() => {
-    window.addEventListener('scroll', toggleVisbility)
-    return () => {
-      window.removeEventListener('scroll', toggleVisbility)
-    }
-  }, [])
 
   return (
     <>
@@ -42,21 +29,25 @@ export default function Navbar() {
         />
       </div>
       <nav
-        className={`${style.nav} ${isVisible ? style.showBg : style.hideBg} ${
-          hamburgerOpen ? style.open : style.closed
-        }`}
+        className={`${style.nav} ${hamburgerOpen ? style.open : style.closed}`}
       >
         <div className={style.innerNav}>
           <div className={style.home}>
             <Link href="#home" passHref>
-              <a className={style.link} onClick={toggleHamburger}>
-                home
+              <a
+                className={`${style.link} ${style.homelink}`}
+                onClick={toggleHamburger}
+              >
+                ac
               </a>
             </Link>
           </div>
           <div className={style.menu}>
             <Link href="#work" passHref>
-              <a className={style.link} onClick={toggleHamburger}>
+              <a
+                className={`${style.link} ${style.active}`}
+                onClick={toggleHamburger}
+              >
                 work
               </a>
             </Link>
