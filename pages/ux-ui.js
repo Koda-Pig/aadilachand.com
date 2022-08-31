ccbeb2435dea8c34534b4525a9efae2b2474a2f1
Josh Koter <josh.koter@gmail.com>
2022-08-31 23:20:54 +0200

    Redid the ux ui page to match mockup. Removed replaced components and related files.

--- pages/ux-ui.js
+++ pages/ux-ui.js
@@ -1,12 +1,17 @@
 import { useRef } from 'react'
+import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Link from 'next/link'
+
 import Footer from '../components/Footer'
-import VaaibPart from '../components/ux-ui/VaaibPart'
-import EarthdirtPart from '../components/ux-ui/EarthdirtPart'
-import StyleExplorationPart from '../components/ux-ui/StyleExplorationPart'
 import CustomHead from '../components/CustomHead'
 import ScrollToTop from '../components/ScrollToTop'
 import styles from '../styles/ux-ui/uxUi.module.scss'
+import vaaibImg from '../public/images/ux-ui/vibe-phone.png'
+import vaaibTxt from '../public/images/ux-ui/vaaib.svg'
+import earthdirtImg from '../public/images/ux-ui/earthdirt-laptop.png'
+import tuneImg from '../public/images/ux-ui/tune-phone.png'
+import tuneTxt from '../public/images/ux-ui/tune.svg'
 
 export default function UxUi() {
   const container = useRef(null)
@@ -18,9 +23,62 @@ export default function UxUi() {
       <div className={styles.container} ref={container}>
         <h1 className={styles.pageHeading}>ux/ui</h1>
         <main>
-          <VaaibPart />
-          <EarthdirtPart />
-          <StyleExplorationPart />
+          <section className={styles.images}>
+            <div className={styles.vaaib}>
+              <div className={styles.heading}>
+                <Image src={vaaibTxt} quality={100} alt="" />
+              </div>
+              <div className={styles.img}>
+                <Image src={vaaibImg} quality={100} alt="" />
+              </div>
+            </div>
+            <div className={styles.earthdirt}>
+              <h2 className={styles.heading}>earthdirt</h2>
+              <div className={styles.img}>
+                <Image src={earthdirtImg} quality={100} alt="" />
+              </div>
+            </div>
+            <div className={styles.tune}>
+              <div className={styles.heading}>
+                <Image src={tuneTxt} quality={100} alt="" />
+              </div>
+              <div className={styles.img}>
+                <Image src={tuneImg} quality={100} alt="" />
+              </div>
+            </div>
+          </section>
+          <section className={styles.writeups}>
+            <div className={styles.vaaib}>
+              <p>
+                <strong>case study</strong>
+                <br />A very small stage in a vast cosmic arena worldlets star
+                stuff harvesting star light
+              </p>
+              <Link href="#" passHref>
+                <a className={styles.callToAction}>view project</a>
+              </Link>
+            </div>
+            <div className={styles.earthdirt}>
+              <p>
+                <strong>case study</strong>
+                <br />A very small stage in a vast cosmic arena worldlets star
+                stuff harvesting star light
+              </p>
+              <Link href="/ux-ui/earthdirt" passHref>
+                <a className={styles.callToAction}>view project</a>
+              </Link>
+            </div>
+            <div className={styles.tune}>
+              <p>
+                <strong>case study</strong>
+                <br />A very small stage in a vast cosmic arena worldlets star
+                stuff harvesting star light
+              </p>
+              <Link href="/ux-ui/tune" passHref>
+                <a className={styles.callToAction}>view project</a>
+              </Link>
+            </div>
+          </section>
         </main>
         <Footer element={container} />
       </div>
